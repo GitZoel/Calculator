9a5f6b6c3977938c886c0a9d572279a3f2176207
Zoel Lindstrom <zoelsburgers@gmail.com>
2022-06-06 17:48:11 -0500

    commit before I add the operations

---
+++ script.js
@@ -0,0 +1,54 @@
+const calculator = document.querySelector('.calculator');
+const keys = calculator.querySelector('.calculator__keys');
+
+keys.addEventListener('click', e => {
+ if (e.target.matches('button')) {
+    const key = e.target;
+    const action = key.dataset.action;
+    const keyContent = key.textContent
+    const outputNum = output.textContent;
+        if (!action) {
+            if (outputNum === '0'){
+                output.textContent = keyContent;
+            } else if (outputNum === '-0') {
+                output.textContent = '-' + keyContent;
+            } else {
+                output.textContent = outputNum + keyContent;
+            }
+        } else {
+            switch (action) {
+                // data action keys
+                case 'decimal':
+                    console.log('decimal');
+                    //only allows one decimal per string
+                    if (!outputNum.includes('.')) {
+                        output.textContent = outputNum + '.'
+                    } else {
+                        console.log(output.textContent);
+                    }
+                    break;
+                case 'clear':
+                    console.log('clear');
+                    output.textContent = 0;
+                    break;
+                case 'calculate':
+                    console.log('calculate');
+                    break;
+                case 'neg':
+                    console.log('neg/pos');
+                    if (outputNum.includes('-')) {
+                        output.textContent = outputNum.slice(1);
+                    } else { //if (output.textContent !== '0') 
+                        output.textContent = '-' + outputNum;
+                    }
+                    break;
+                case 'percent':
+                    output.textContent = outputNum / 100;
+                default: 
+                    console.log(`${action}`)
+                    
+            }
+        }
+ }
+})
+
